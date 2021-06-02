7ca02c35905c5ae768beef33f97cd1dff782f8e5
Mr-DOY <83167364+Mr-DOY@users.noreply.github.com>
2021-06-02 12:26:08 +0700

    Add files via upload

---
+++ apaini.sh
@@ -0,0 +1,50 @@
+ #!/bin/bash
+ #project01
+
+clear
+ printf "
+
+                                            tools : 404:')
+ _________________________________________________________
+            [+] AUTHOR : Mr.DOY / Mrs.ADOY [+]
+            [+] TEAM   : DOY X-PUNTEN      [+]
+            [+] INSTAGRAM : @doy_xpunten_  [+]
+ _________________________________________________________
+            [+] JANGAN SOMBONG,            [+]
+            [+]        DIMATA TUHAN.       [+]
+            [+]               KITA SAMA AE [+]
+ _________________________________________________________
+            [+] DAH LAH KELAMAAN INTRO     [+]
+            [+]             MENDING LOGIN. [+]
+ _________________________________________________________
+                     [+] WARNING [+]
+            sebelum login mending lu follow ig
+                      @doy_xpunten_
+ _________________________________________________________
+                »»»»»»»»» Login »»»»»»»»»
+ "
+
+
+
+
+ read -p "{USERNAME} :" user
+ if [ $user = "AOC" ]; then
+ clear
+ else
+ printf "
+           uppsss.....Login Gagal :(
+ "
+ fi
+
+ read -p "{PASSWORD} :" pass
+ if [ $pass = "AOC" ]; then
+ clear
+ else
+ printf "
+          uppssss.......Login Gagal :(
+ "
+ fi
+
+ printf "
+          SELAMAT LOGIN SUKSES :)
+ "
